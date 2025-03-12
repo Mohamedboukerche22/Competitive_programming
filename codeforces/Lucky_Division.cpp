79a7f7446a36f3362e39f2360eb0607524eb54cf
Mohamedboukerche22 <mohamedboukerche55@gmail.com>
2025-03-12 01:49:48 +0100

    Create Lucky_Division.cpp

---
+++ codeforces/Lucky_Division.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+     
+int  main(){
+  int n;
+  cin >>n;
+  if(n%47==0||n%477==0||n%744==0||n%4==0||n%7==0){
+     cout <<"YES";
+      }else {
+        cout <<"NO";
+      }
+    }
