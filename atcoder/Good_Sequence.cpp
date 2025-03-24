3c1c4f20c5065d5ab17867b7b15a59b0065c63ef
Mohamedboukerche22 <mohamedboukerche55@gmail.com>
2025-03-24 15:08:54 +0100

    Create Good_Sequence.cpp

---
+++ atcoder/Good_Sequence.cpp
@@ -0,0 +1,23 @@
+//hiii
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int n;
+   cin >>n;
+   unordered_map<int,int> lol;
+   for(int i = 0;i <n;i++){
+      int c;
+      cin >>c;
+      lol[c]++;  
+   }
+   int count = 0;
+   for(auto &i : lol){
+      if(i.second > i.first){
+         count += i.second - i.first;
+      }else if (i.first > i.second){
+         count += i.second;
+      }
+   }
+   cout << count << endl;
+
+}
