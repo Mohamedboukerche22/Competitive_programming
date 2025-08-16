9a804d4da1ee533fe3c83423eff82d5dadb79529
Mohamedboukerche22 <mohamedboukerche55@gmail.com>
2025-08-16 13:32:47 +0100

    Create 231. Power of Two.py

---
+++ leetcode/231. Power of Two.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/power-of-two/
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        for i in range(100000):
+            if 2**i == n or n == 1:
+                return True
+            if 2**i > n:
+                break    
+        return False    
