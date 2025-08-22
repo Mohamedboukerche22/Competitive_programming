d38ddf2504d07641b3adccbc44a26be13294fcbd
Mohamedboukerche22 <mohamedboukerche55@gmail.com>
2025-08-22 21:40:18 +0100

    Update a.cpp

--- DM::OJ/a.cpp
+++ DM::OJ/a.cpp
@@ -1,9 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     long long  n;
+    
     cin >>n;
+    
     long long res = n-2;
+    
     long long jj = (res+n)/2;
+    
     cout<<jj;
 }
