455a5e4dee38a0de8f4050c4fd675828fe0e24aa
Mohamed Boukerche <mohamedboukerche55@gmail.com>
2025-09-22 22:28:25 +0100

    Update task-8.cpp

--- AOI-contsests/inca-1/task-8.cpp
+++ AOI-contsests/inca-1/task-8.cpp
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define fastAOI ios::sync_with_stdio(false); cin.tie(nullptr);
 #define ll long long
 #define all(x) (x).begin(), (x).end()
 #define pb push_back
 #define sz(x) (int)(x).size()
 const int N = 50;
-
-
 int main() {
     fastAOI;
     vector<long long>dp(N);
@@ -26,37 +23,18 @@ int main() {
         for(int &i : fibo){
             cin >> i;
         }
-        //vector<pair<int,char>>ar(n);
         string str;
         cin.ignore();
         getline(cin,str);
-       /* vector<char>arr;
-        for(char i : str){
-            arr.pb(i);
-        }*/
         vector<pair<char,int>> chars;
         int j = 0; 
-        /*for (int i = 0; i < (int)str.size(); i++) {
-           if ('A' <= chars[i].first && chars[i].first <= 'Z') { 
-                if (j < n) {
-                    chars.push_back({chars[i].first, fibo[j]});
-                    j++;
-                } 
-            }
-          if (j < (int)fibo.size()) {
-         chars.push_back({str[i], fibo[j]}); j++;
-            }
-         }*/
-
         for (char c : str) {
             if ('A' <= c && c <= 'Z') { 
                 if (j < n) chars.pb({c, fibo[j]}); j++;
             }
         }
-         //sort(chars.begin(), chars.end());
     sort(chars.begin(), chars.end(), [](auto &a, auto &b){
-    return a.second < b.second;
-    });
+    return a.second < b.second;});
     ll mx = *max_element(all(fibo));
     
     ll mxdp = find(all(dp), mx) - dp.begin();
@@ -64,24 +42,12 @@ int main() {
     int g = 0;
     for(int i = 0 ; i <= mxdp && g<sz(chars);i++){
         if((chars[g].second) == dp[i]){
-            //ans.pb(chars[g].first);
             ans[i] = chars[g].first;g++;
-        }//else{
-           // ans.pb(' ');
-       // }
+        }  
     }
-    /*for(char i :ans){
-        cout <<i;
-    }*/
         string out(ans.begin(), ans.end());
         while (!out.empty() && out.back() == ' ') out.pop_back();
-        cout << out << "\n";
-
-    /*for(int i = 0 ; i< chars.size();i++){
-        cout <<chars[i].first<<" : "<<chars[i].second<<endl;
-    }*/
-
-       
+        cout << out << "\n";     
     }
     return 0;
 }
